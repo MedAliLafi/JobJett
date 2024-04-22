f5fb1a13a88c89888f1fcebd4334f808aa018292
AzizCoding02 <belhajyahiaaziz@gmail.com>
2024-04-22 01:35:14 +0100

    4/22/2024

--- Code/jobjett/src/components/candidate/CandidateHomePage/CandidateHomePage.jsx
+++ Code/jobjett/src/components/candidate/CandidateHomePage/CandidateHomePage.jsx
@@ -192,7 +192,8 @@ function CandidateHomePage() {
             }) => (
               <div
                 key={JobOfferID}
-                className="singleJob p-[20px] bg-white rounded-[10px] hover:bg-blueColor shadow-lg shadow-greyIsh-400/700 hover:shadow-lg"
+                id="singleJob"
+                className="singleJob p-[20px]  rounded-[10px] hover:bg-blueColor hover:text-white shadow-lg shadow-greyIsh-400/700 hover:shadow-lg child"
               >
                 <span className="flex justify-between items-center gap-4">
                   <h1 className="text-[16px] font-semibold text-black group-hover:text-white">
@@ -213,90 +214,19 @@ function CandidateHomePage() {
                     </span>
                   </div>
                 </div>
-                <div className="jobContent">{/* Job offer details */}</div>
+
                 {/* Buttons */}
                 <div className="jobButtons">
                   <button
                     data-modal-target="popup-modal"
-                    className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-black hover:bg-white group-hover/item:text-black group-hover:text-white"
+                    className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-black hover:bg-white group-hover/item:text-white group-hover:text-white"
                     onClick={toggleModal}
                   >
                     See Details
                   </button>
-                  {showModal && (
-                    <div
-                      id="popup-modal"
-                      tabIndex="-1"
-                      className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50"
-                      onClick={toggleModal}
-                    >
-                      <div className="relative p-4 w-full max-w-md max-h-full">
-                        <div className="relative bg-white rounded-lg shadow">
-                          <button
-                            type="button"
-                            className="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
-                            data-modal-hide="popup-modal"
-                            onClick={toggleModal}
-                          >
-                            <svg
-                              className="w-3 h-3"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 14 14"
-                            >
-                              <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                              />
-                            </svg>
-                            <span className="sr-only">Close modal</span>
-                          </button>
-                          <div className="p-4 md:p-5 text-center">
-                            <svg
-                              className="mx-auto mb-4 text-gray-400 w-12 h-12"
-                              aria-hidden="true"
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                              />
-                            </svg>
-                            <h3 className="mb-5 text-lg font-normal text-gray-500">
-                              Are you sure you want to delete this product?
-                            </h3>
-                            <button
-                              data-modal-hide="popup-modal"
-                              type="button"
-                              className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
-                              onClick={toggleModal}
-                            >
-                              Yes, I'm sure
-                            </button>
-                            <button
-                              data-modal-hide="popup-modal"
-                              type="button"
-                              className="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100"
-                              onClick={toggleModal}
-                            >
-                              No, cancel
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  )}
+
                   <button
-                    className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-black hover:bg-white group-hover/item:text-black group-hover:text-white "
+                    className="border-[2px] rounded-[10px] block p-[10px] w-full text-[14px] font-semibold text-black hover:bg-white group-hover/item:text-white group-hover:text-white "
                     onClick={() =>
                       navigate(
                         `/candidate/application?jobofferId=${JobOfferID}`
--- Code/jobjett/src/components/candidate/CandidateRegister/CandidateRegister.jsx
+++ Code/jobjett/src/components/candidate/CandidateRegister/CandidateRegister.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import logo from "../../../assets/output-onlinetools.png";
 import "./CandidateRegister.css";
